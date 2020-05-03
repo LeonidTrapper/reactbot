8542f0be21903c2f215365e8e980e84e23803931
Leonid <46868535+LeonidTrapper@users.noreply.github.com>
2020-05-03 16:39:50 +0300

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,14 +1,57 @@
-const Discord = require('discord.js');
-const bot = new Discord.Client();
-const cfg = require(`./config.json`);
-const token = cfg.token;
-const prefix = cfg.prefix;
+const Discord 	 = require('discord.js');
+const bot		 = new Discord.Client();
+const cfg 		 = require(`./config.json`);
+const fs         = require("fs");
+bot.commands     = new Discord.Collection();
+const token 	 = cfg.token;
+const prefix 	 = cfg.prefix;
 
 bot.login(process.env.token).then(() => console.log(`Login|`,`Бот активен\nТег бота: ${bot.user.tag}`)).catch(err => { console.log(`Login|`,`Произошла ошибка. Напиши мне в лс Leonid#9085`), console.log(err) })
 
 
 
 
+
+//////////////////////////////////////  Не трогай  ////////////////////////////////////////////
+
+
+// fs
+fs.readdir("./cmd", (err, files) => {
+	if(err) console.log(err);
+	let jsfile = files.filter(f => f.split(".").pop() === "js");
+
+// bot cmds set
+jsfile.forEach((f, i) => {
+	let props = require(`./cmd/${f}`);
+	bot.commands.set(props.help.name, props);
+});
+
+})
+
+// Проверка автора сообщения, текстового канала, команды.
+bot.on("message", async message => {
+if(message.author.bot) return;
+if(message.channel.type === "dm") return;
+let messageArray = message.content.split(" ");
+let cmd          = messageArray[0];
+let args 		 = messageArray.slice(1);
+
+
+let commandfile = bot.commands.get(cmd.slice(prefix.length));
+if(commandfile) commandfile.run(bot,message,args);
+});
+
+
+//////////////////////////////////////  Не трогай  ////////////////////////////////////////////
+
+
+
+
+
+
+
+
+// Хуйня
 bot.on(`message`, async (message) => {
 	// Ставит реакцию в аестетик
 	if(message.channel.id === `596047655336017921`) {
@@ -30,7 +73,6 @@ bot.on(`message`, async (message) => {
 		message.react(emoji);
 	}
 })
-
 
 
 
