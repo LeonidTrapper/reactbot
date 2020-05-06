f2af61fb126a7e5c3c4d573309428b629510c2ac
Leonid <46868535+LeonidTrapper@users.noreply.github.com>
2020-05-06 22:17:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -77,9 +77,7 @@ bot.on(`message`, async (message) => {
 	}
 })
 
-bot.on('guildMemberAdd', (guildMember) => {
-   guildMember.addRole(guildMember.guild.roles.find(role => role.name === "⭐ꜱᴛᴀʀ⭐")).catch(err => {})
-})
+
 
 bot.on(`message`, async (message) => {
 
